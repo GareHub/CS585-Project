3b2ea700b4ca13c696d7cfa133e5a082baa3f496
Garrett Martin <martin.garrett132@gmail.com>
2019-10-24 01:19:05 -0400

    Created data_to_vector

---
+++ data_to_vector.py
@@ -0,0 +1,25 @@
+import csv, os
+import string
+
+'''
+data2vec takes in a string 'dirname', which is a directory name of CSVs.
+It returns a dictionary, with each key being a file name within the dictionary,
+and the keys being a list of sequences, or lists of individual words.
+'''
+def data2vec(dirname): 
+    directory = os.listdir(dirname)
+    class_dictionary = {}
+    for filename in directory:
+        name = "Data/" + filename
+        with open(name) as current_data_set:
+            csv_reader = list(csv.reader(current_data_set, delimiter = ','))
+            csv_reader = csv_reader[4:]
+            class_dictionary[filename] = []
+            for x in csv_reader:
+                x[2] = x[2].lower()
+                x[3] = x[3].lower()
+                sequence = x[2:3] + (x[3].translate(None, string.punctuation).split())
+                sequence = sequence[:-1]
+                class_dictionary[filename].append(sequence)
+
+    return class_dictionary
