c84bbc5a672e98c7ed34d548e9d36f57f218a42f
Vincent Pietropaolo <vpietropaolo@umass.edu>
2019-11-20 16:30:23 -0500

    Working baseline (GloVe)

--- baseline_model.py
+++ baseline_model.py
@@ -6,12 +6,15 @@ import nltk
 import numpy as np
 import pickle
 
-EPOCH = 15
+EPOCH = 10
 BATCH_SIZE = 1
 INPUT_SIZE = 100
-LR = 0.1
+LR = .0003
+langs = ["Chinese", "French", "Greek", "Italian", "Portugese", "European Spanish", "Latin American Spanish"]
+lang_count = len(langs)
 
 torch.manual_seed(1)
+np.random.seed(1)
 
 class RNN(nn.Module):
     def __init__(self):
@@ -23,14 +26,15 @@ class RNN(nn.Module):
             num_layers=1,
             batch_first=True,
         )
-
+        self.f1 = torch.tanh
         self.out = nn.Linear(100, 7)
 
     def forward(self, x):
         # x shape (batch, time_step, input_size)
         # h_n shape (n_layers, batch, hidden_size)
         r_out, h_n = self.rnn(x, None)
-        out = self.out(r_out[:, -1, :])
+        h1 = self.f1(r_out)
+        out = self.out(h1[:, -1, :])
         return out
 
 with open("word_vecs.txt", 'rb') as f:
@@ -40,13 +44,13 @@ rnn = RNN()
 optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
 loss_func = F.cross_entropy
 
-langs = ["Chinese", "French", "Greek", "Italian", "Portugese", "European Spanish", "Latin American Spanish"]
 d = []
-for l in range(len(langs)):
+for l in range(lang_count):
 	d += [ [line, l] for line in data[langs[l]] ]
 np.random.shuffle(d)
-test = d[len(d)//2:]
-train = d[:len(d)//2]
+train = d[:3*len(d)//5]
+val = d[3*len(d)//5:4*len(d)//5]
+test = d[4*len(d)//5:]
 for epoch in range(EPOCH):
 	loss_out = 0
 	y_right = 0
@@ -59,19 +63,31 @@ for epoch in range(EPOCH):
 
 		optimizer.zero_grad()
 		output = rnn(b_x)
+
 		loss = loss_func(output, b_y)
 		loss.backward()
 		optimizer.step()
 
 		loss_out += loss.item()
-
-		b_x = torch.tensor([b[0] for b in test[i:i+BATCH_SIZE]])
-		b_y = torch.tensor([b[1] for b in test[i:i+BATCH_SIZE]])
+	for i in range(0,len(val),BATCH_SIZE):
+		b_x = torch.tensor([b[0] for b in val[i:i+BATCH_SIZE]])
+		b_y = torch.tensor([b[1] for b in val[i:i+BATCH_SIZE]])
 		
 		test_output = rnn(b_x)
-		pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
-		#print(dict(nltk.FreqDist(pred_y)), loss.item())
-		y_right += pred_y == l
+		pred_y = torch.max(test_output, 1)[1].data.numpy()
+		#print(pred_y, b_y.tolist())
+		y_right += pred_y == b_y.tolist()
 		y_total += 1
 	accuracy = y_right/y_total
-	print('Epoch: ', epoch, '| train loss:', loss_out, '| test accuracy: %.2f' % accuracy)
+	print('Epoch: ', epoch, '| train loss:', loss_out, '| validation accuracy: %.2f' % accuracy)
+for i in range(0,len(val),BATCH_SIZE):
+	b_x = torch.tensor([b[0] for b in test[i:i+BATCH_SIZE]])
+	b_y = torch.tensor([b[1] for b in test[i:i+BATCH_SIZE]])
+	
+	test_output = rnn(b_x)
+	pred_y = torch.max(test_output, 1)[1].data.numpy()
+	#print(pred_y, b_y.tolist())
+	y_right += pred_y == b_y.tolist()
+	y_total += 1
+accuracy = y_right/y_total
+print('Test accuracy: %.2f' % accuracy)
