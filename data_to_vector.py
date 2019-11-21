75d25d0538e46458f3153a947b5dd4dcb7cbcb8f
Hadley Pope <hpope@umass.edu>
2019-11-20 19:30:02 -0500

    Updated to be Python 3.6 compatible

--- data_to_vector.py
+++ data_to_vector.py
@@ -11,14 +11,14 @@ def data2vec(dirname):
     class_dictionary = {}
     for filename in directory:
         name = "Data/" + filename
-        with open(name) as current_data_set:
+        with open(name, encoding='utf-8') as current_data_set:
             csv_reader = list(csv.reader(current_data_set, delimiter = ','))
             csv_reader = csv_reader[4:]
             class_dictionary[filename] = []
             for x in csv_reader:
                 x[2] = x[2].lower()
                 x[3] = x[3].lower()
-                sequence = x[2:3] + (x[3].translate(None, string.punctuation).split())
+                sequence = x[2:3] + (x[3].strip(string.punctuation).split())
                 sequence = sequence[:-1]
                 class_dictionary[filename].append(sequence)
 
